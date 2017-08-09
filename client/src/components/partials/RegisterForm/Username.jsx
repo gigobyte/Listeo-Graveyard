4e2d03d0ef8da4bde4e4edef928523967b34c262
gigobyte <gigobest2@gmail.com>
2017-08-09 22:56:57 +0300

    Textfield styling

--- client/src/components/partials/RegisterForm/Username.jsx
+++ client/src/components/partials/RegisterForm/Username.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import InputField from '../../shared/InputField'
 import createRegisterFormField from './createRegisterFormField'
 
-const Username = ({ value, updateField }) =>
-    <InputField value={value} onChange={updateField} />
+const Username = ({ value, updateField, validateField }) =>
+    <InputField placeholder="Username" value={value} onChange={updateField} onBlur={validateField} />
 
 export default createRegisterFormField('username', Username)
--- client/src/components/shared/InputField.jsx
+++ client/src/components/shared/InputField.jsx
@@ -1,15 +1,76 @@
 import React from 'react'
 import styled from 'styled-components'
+import { T, F } from 'ramda'
+import { withState } from 'recompose'
 
 const Input = styled.input`
-    background-color: transparent;
-    border-left: none;
-    border-top: none;
-    border-right: none;
-    border-bottom: 1px solid white;
+    padding: 0px;
+    position: relative;
+    width: 100%;
+    border: medium none;
+    outline: medium none;
+    background-color: rgba(0, 0, 0, 0);
+    color: rgba(0, 0, 0, 0.87);
+    cursor: inherit;
+    font: inherit;
+    height: 100%;
 `
 
-const InputField = (props) =>
-    <Input type="text" {...props} />
+const Wrapper = styled.div`
+    font-size: 16px;
+    line-height: 24px;
+    height: 48px;
+    display: inline-block;
+    position: relative;
+    background-color: transparent;
+`
 
-export default InputField
+const Border = styled.hr`
+    border-width: medium medium 1px;
+    border-style: none none solid;
+    border-color: rgb(224, 224, 224);
+    bottom: 8px;
+    box-sizing: content-box;
+    margin: 0px;
+    position: absolute;
+    width: 100%;
+`
+
+const Placeholder = styled.div`
+    position: absolute;
+    opacity: ${props => props.show ? 1 : 0};
+    color: rgba(0, 0, 0, 0.3);
+    transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+    bottom: 12px;
+`
+
+const BorderFocused = styled.div`
+    border-width: medium medium 2px;
+    border-style: none none solid;
+    border-color: rgb(0, 188, 212);
+    bottom: 8px;
+    box-sizing: content-box;
+    margin: 0px;
+    position: absolute;
+    width: 100%;
+    transform: scaleX(${props => props.show ? 1 : 0});
+    transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+`
+
+const InputField = ({value, onChange, onBlur=T, onFocus=T, placeholder, focused, setFocused }) =>
+    <Wrapper>
+        <Input
+            type="text"
+            value={value}
+            onChange={onChange}
+            onBlur={() => (onBlur(), setFocused(F))}
+            onFocus={() => (onFocus(), setFocused(T))}
+        />
+        <div>
+            <Placeholder show={!value}>{placeholder}</Placeholder>
+            <Border />
+            <BorderFocused show={focused}/>
+        </div>
+    </Wrapper>
+
+export default withState('focused', 'setFocused', false)(InputField)
